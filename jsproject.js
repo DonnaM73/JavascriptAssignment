13643623962e57414e4268bd01bb3122b76fb142
DonnaM73 <donna.airoldi@journalism.cuny.edu>
2016-02-14 00:36:41 -0500

    Add jsindex.html

---
+++ jsproject.js
@@ -0,0 +1,13 @@
+var x = 25;
+var y = 5;
+
+var a = x + y
+var s = x - y
+var m = x * y
+var d = x / y
+
+document.getElementById("add").innerHTML = "When I add x and y I get: " + a;
+document.getElementById("subt").innerHTML = "When I subtract y from x I get: " + s;
+document.getElementById("mult").innerHTML = "When I multiply x and y I get: " + m;
+document.getElementById("div").innerHTML = "When I divide x by y I get: " + d;
+
